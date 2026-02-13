356d02058f132758fe2aebb1ca44a03fd25dca9f
Marc-Antoine ARNAUD <marc-antoine.arnaud@luminvent.com>
2026-02-13 15:00:40 +0100

    fix: deserialize byte to long from xsd type and integer

--- src/datatypes/de.rs
+++ src/datatypes/de.rs
@@ -10,113 +10,109 @@ use crate::{
 };
 
 macro_rules! deserialize_datatype {
-	($($ty:ty : $iri:ident),*) => {
-		$(
-			impl<V: Vocabulary, I: Interpretation> LinkedDataDeserializeSubject<I, V> for $ty
+	($ty:ty, $iri:ident, $($extra_type:expr)*) => {
+		impl<V: Vocabulary, I: Interpretation> LinkedDataDeserializeSubject<I, V> for $ty
+		where
+			V: LiteralVocabulary,
+			I: ReverseIriInterpretation<Iri = V::Iri> + ReverseLiteralInterpretation<Literal = V::Literal>
+		{
+			fn deserialize_subject_in<D>(
+				vocabulary: &V,
+				interpretation: &I,
+				_dataset: &D,
+				_graph: Option<&I::Resource>,
+				resource: &I::Resource,
+				context: Context<I>
+			) -> Result<Self, FromLinkedDataError>
 			where
-				V: LiteralVocabulary,
-				I: ReverseIriInterpretation<Iri = V::Iri> + ReverseLiteralInterpretation<Literal = V::Literal>
+				D: rdf_types::dataset::PatternMatchingDataset<Resource = I::Resource>
 			{
-				fn deserialize_subject_in<D>(
-					vocabulary: &V,
-					interpretation: &I,
-					_dataset: &D,
-					_graph: Option<&I::Resource>,
-					resource: &I::Resource,
-					context: Context<I>
-				) -> Result<Self, FromLinkedDataError>
-				where
-					D: rdf_types::dataset::PatternMatchingDataset<Resource = I::Resource>
-				{
-					let mut literal_ty = None;
-					for l in interpretation.literals_of(resource) {
-						let l = vocabulary.literal(l).unwrap();
-						match l.type_ {
-							rdf_types::LiteralTypeRef::Any(ty_iri) => {
-								let ty_iri = vocabulary.iri(ty_iri).unwrap();
-								if ty_iri == xsd_types::$iri {
-									return match l.value.parse() {
-										Ok(value) => Ok(value),
-										Err(_) => Err(FromLinkedDataError::InvalidLiteral(
-											context.into_iris(
-												vocabulary,
-												interpretation
-											)
-										))
-									}
+				let mut literal_ty = None;
+				for l in interpretation.literals_of(resource) {
+					let l = vocabulary.literal(l).unwrap();
+					match l.type_ {
+						rdf_types::LiteralTypeRef::Any(ty_iri) => {
+							let ty_iri = vocabulary.iri(ty_iri).unwrap();
+							if ty_iri == xsd_types::$iri $(|| ty_iri == $extra_type)* {
+								return match l.value.parse() {
+									Ok(value) => Ok(value),
+									Err(_) => Err(FromLinkedDataError::InvalidLiteral(
+										context.into_iris(
+											vocabulary,
+											interpretation
+										)
+									))
 								}
-
-								literal_ty = Some(ty_iri)
 							}
-							rdf_types::LiteralTypeRef::LangString(_) => {
-								literal_ty = Some(RDF_LANG_STRING)
-							}
-						}
-					}
 
-					match literal_ty {
-						Some(ty) => {
-							Err(FromLinkedDataError::LiteralTypeMismatch {
-								context: context.into_iris(vocabulary, interpretation),
-								expected: Some(xsd_types::$iri.to_owned()),
-								found: ty.to_owned()
-							})
+							literal_ty = Some(ty_iri)
 						}
-						None => {
-							Err(FromLinkedDataError::ExpectedLiteral(
-								context.into_iris(vocabulary, interpretation)
-							))
+						rdf_types::LiteralTypeRef::LangString(_) => {
+							literal_ty = Some(RDF_LANG_STRING)
 						}
 					}
 				}
-			}
 
-			impl<V: Vocabulary, I: Interpretation> LinkedDataDeserializePredicateObjects<I, V> for $ty
-			where
-				V: LiteralVocabulary,
-				I: ReverseIriInterpretation<Iri = V::Iri> + ReverseLiteralInterpretation<Literal = V::Literal>
-			{
-				fn deserialize_objects_in<'a, D>(
-					vocabulary: &V,
-					interpretation: &I,
-					dataset: &D,
-					graph: Option<&I::Resource>,
-					objects: impl IntoIterator<Item = &'a <I as Interpretation>::Resource>,
-					context: Context<I>
-				) -> Result<Self, FromLinkedDataError>
-				where
-					<I as Interpretation>::Resource: 'a,
-					D: rdf_types::dataset::PatternMatchingDataset<Resource = I::Resource>
-				{
-					let mut error = None;
-
-					for o in objects {
-						match Self::deserialize_subject_in(vocabulary, interpretation, dataset, graph, o, context) {
-							Ok(value) => return Ok(value),
-							Err(e) => error = Some(e)
-						}
+				match literal_ty {
+					Some(ty) => {
+						Err(FromLinkedDataError::LiteralTypeMismatch {
+							context: context.into_iris(vocabulary, interpretation),
+							expected: Some(xsd_types::$iri.to_owned()),
+							found: ty.to_owned()
+						})
 					}
-
-					Err(error.unwrap_or_else(|| {
-						FromLinkedDataError::MissingRequiredValue(
+					None => {
+						Err(FromLinkedDataError::ExpectedLiteral(
 							context.into_iris(vocabulary, interpretation)
-						)
-					}))
+						))
+					}
 				}
 			}
-		)*
+		}
+
+		impl<V: Vocabulary, I: Interpretation> LinkedDataDeserializePredicateObjects<I, V> for $ty
+		where
+			V: LiteralVocabulary,
+			I: ReverseIriInterpretation<Iri = V::Iri> + ReverseLiteralInterpretation<Literal = V::Literal>
+		{
+			fn deserialize_objects_in<'a, D>(
+				vocabulary: &V,
+				interpretation: &I,
+				dataset: &D,
+				graph: Option<&I::Resource>,
+				objects: impl IntoIterator<Item = &'a <I as Interpretation>::Resource>,
+				context: Context<I>
+			) -> Result<Self, FromLinkedDataError>
+			where
+				<I as Interpretation>::Resource: 'a,
+				D: rdf_types::dataset::PatternMatchingDataset<Resource = I::Resource>
+			{
+				let mut error = None;
+
+				for o in objects {
+					match Self::deserialize_subject_in(vocabulary, interpretation, dataset, graph, o, context) {
+						Ok(value) => return Ok(value),
+						Err(e) => error = Some(e)
+					}
+				}
+
+				Err(error.unwrap_or_else(|| {
+					FromLinkedDataError::MissingRequiredValue(
+						context.into_iris(vocabulary, interpretation)
+					)
+				}))
+			}
+		}
 	};
 }
 
-deserialize_datatype!(
-	u8: XSD_UNSIGNED_BYTE,
-	u16: XSD_UNSIGNED_SHORT,
-	u32: XSD_UNSIGNED_INT,
-	u64: XSD_UNSIGNED_LONG,
-	i8: XSD_BYTE,
-	i16: XSD_SHORT,
-	i32: XSD_INT,
-	i64: XSD_LONG,
-	String: XSD_STRING,
-	xsd_types::DateTime: XSD_DATE_TIME
-);
+deserialize_datatype!(u8, XSD_UNSIGNED_BYTE, xsd_types::XSD_INTEGER);
+deserialize_datatype!(u16, XSD_UNSIGNED_SHORT, xsd_types::XSD_INTEGER);
+deserialize_datatype!(u32, XSD_UNSIGNED_INT, xsd_types::XSD_INTEGER);
+deserialize_datatype!(u64, XSD_UNSIGNED_LONG, xsd_types::XSD_INTEGER);
+deserialize_datatype!(i8, XSD_BYTE, xsd_types::XSD_INTEGER);
+deserialize_datatype!(i16, XSD_SHORT, xsd_types::XSD_INTEGER);
+deserialize_datatype!(i32, XSD_INT, xsd_types::XSD_INTEGER);
+deserialize_datatype!(i64, XSD_LONG, xsd_types::XSD_INTEGER);
+deserialize_datatype!(String, XSD_STRING,);
+deserialize_datatype!(xsd_types::DateTime, XSD_DATE_TIME,);
